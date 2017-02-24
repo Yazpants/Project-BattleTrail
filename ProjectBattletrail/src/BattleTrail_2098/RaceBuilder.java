5762f2415fec77478d8ae7f57fd30cc94498d667
Yazpants <Victoriusify@gmail.com>
2017-02-24 19:36:20 +0100

    UPDATE: Races and stats

--- ProjectBattletrail/src/BattleTrail_2098/RaceBuilder.java
+++ ProjectBattletrail/src/BattleTrail_2098/RaceBuilder.java
@@ -9,11 +9,22 @@ package BattleTrail_2098;
  */
 public class RaceBuilder {
     Race human;
+    Race cyborg;
+    Race martian;
+    Race robot;
+    Race mutant;
 
 
     RaceBuilder() {
         // Test hardcoded race
         human = new Race("Human", 10,10,10,10);
+        cyborg = new Race("Cyborg", 11, 12, 8, 9 );
+        martian = new Race("Martian", 7, 14, 8,11);
+        robot = new Race("Robot", 13, 11, 10, 6);
+        mutant = new Race("Mutant", 9, 9, 13, 9);
+
+
+
     }
 
 }
