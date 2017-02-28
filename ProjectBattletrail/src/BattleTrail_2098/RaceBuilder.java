11088970d01f486cc03481fc95119868decc4191
Unknown <empa.win@gmail.com>
2017-02-28 12:59:32 +0100

    Streamlining the race constructor
    * Races adding is now handled by seprate private method
    * Races added will also be added in the arrayList on creation
    * Check if race exists will now ensure the string is in all caps so we don't have to check elsewhere.

--- ProjectBattletrail/src/BattleTrail_2098/RaceBuilder.java
+++ ProjectBattletrail/src/BattleTrail_2098/RaceBuilder.java
@@ -19,25 +19,45 @@ public class RaceBuilder {
     ArrayList<String> raceList = new ArrayList<>();
 
 
+    /**
+     * Constructor
+     */
     RaceBuilder() {
-        // Test hardcoded race
-        human = new Race("Human", 10,10,10,10);
-        cyborg = new Race("Cyborg", 11, 12, 8, 9 );
-        martian = new Race("Martian", 7, 14, 8,11);
-        robot = new Race("Robot", 13, 11, 10, 6);
-        mutant = new Race("Mutant", 9, 9, 13, 9);
-        raceList.add("HUMAN");
-        raceList.add("CYBORG");
-        raceList.add("MARTIAN");
-        raceList.add("ROBOT");
-        raceList.add("MUTANT");
+        addRace("Human", 10,10,10,10);
+        addRace("Cyborg", 11, 12, 8, 9 );
+        addRace("Martian", 7, 14, 8,11);
+        addRace("Robot", 13, 11, 10, 6);
+        addRace("Mutant", 9, 9, 13, 9);
+    }
 
+    /**
+     * Method which creates a new race object
+     * @param raceName name of race
+     * @param str strength of the race
+     * @param intl intelligence of the race
+     * @param health health of the race
+     * @param luck luckof the race
+     */
+    private void addRace(String raceName, double str, double intl, double health, double luck) {
+
+        // We create the new race object
+        Race newRace = new Race(raceName, str, intl, health, luck);
+
+        // We add the race name to the array list
+        raceName = raceName.toUpperCase();
+        raceList.add(raceName);
 
     }
 
-    public boolean checkIfRaceExist(String race) {
-       return raceList.contains(race);
-
+    /**
+     * Checks if the selected race exist
+     * @param raceName the name of the race
+     * @return true if the race exist
+     */
+    public boolean checkIfRaceExist(String raceName) {
+        //Ensuring that the race name
+        raceName = raceName.toUpperCase();
+        return raceList.contains(raceName);
     }
 
 }
