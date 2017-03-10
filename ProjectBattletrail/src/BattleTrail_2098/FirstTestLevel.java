6e4abc2f3dd1f46f9d0e524fd038b460d963342c
Yazpants <Victoriusify@gmail.com>
2017-03-10 09:42:03 +0100

    Experimenting with first level.

--- ProjectBattletrail/src/BattleTrail_2098/FirstTestLevel.java
+++ ProjectBattletrail/src/BattleTrail_2098/FirstTestLevel.java
@@ -6,27 +6,27 @@ import java.util.Scanner;
  * Created by victor on 2017-03-01.
  */
 public class FirstTestLevel {
-    Player player = new Player();
     Scanner sc = new Scanner(System.in);
 
-    public void firstLevel() {
-        System.out.println("Welcome to Cathcart city " + player.getName() + "This is where you'll begin" +
-                "your mission as a " + player.getRoleName() + "you will be taking out whoever our employer " +
+    public void firstLevel(Player player) {
+        System.out.println("Welcome to Cathcart city\n " + player.getName() + " This is where you'll begin" +
+                " your mission as a " + player.getRoleName() + "\n you will be taking out whoever our employer " +
                 "tells you to. Do you understand?\n [Yes] / [No] ");
 
         String input = sc.next();
-        if (input == "YES") {
+        if (input == "Yes") {
             start();
-        } else if (input == "NO"){
+        } else if (input == "No"){
             System.out.println("right....Let's start either way.\n");
+            start();
         } else {
-            firstLevel();
+            firstLevel(player);
         }
 
     }
     public void start() {
-        System.out.println("Now, this is a huge city so don't get lost." +
-                "your first contact is not far from here, just head a few miles west." +
-                "Okay, you're on your own now.");
+        System.out.println("Now, this is a huge city so don't get lost.\n" +
+                "your first contact is not far from here, just head a few miles west.\n" +
+                "Okay, you're on your own now.\n");
     }
 }
