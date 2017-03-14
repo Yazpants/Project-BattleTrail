b4a51b1d38afad42ff8d5b8a980672a602a74893
Yazpants <Victoriusify@gmail.com>
2017-03-14 20:02:50 +0100

    Trying things out with controller.

---
+++ ProjectBattletrail/src/BattleTrail_2098/UserController.java
@@ -0,0 +1,21 @@
+package BattleTrail_2098;
+
+import java.util.Scanner;
+
+/**
+ * For controlling actions such as directions, and item selection from the later on implemented Inventory system
+ * Test.
+ * Created by victor on 2017-03-14.
+ */
+public class UserController {
+    Scanner sc = new Scanner(System.in);
+
+    public void Directions() {
+
+    }
+    public void Actions() {
+
+    }
+
+
+}
