bbd9cfa88c86f6db25263750908009e0f8ed561a
Yazpants <Victoriusify@gmail.com>
2017-03-14 20:11:40 +0100

    Trying things out with controller.

--- ProjectBattletrail/src/BattleTrail_2098/UserController.java
+++ ProjectBattletrail/src/BattleTrail_2098/UserController.java
@@ -7,15 +7,20 @@ import java.util.Scanner;
  * Test.
  * Created by victor on 2017-03-14.
  */
+
+//An interface to be implemented by UserController for direction events..maybe ¯\_(ツ)_/¯
+interface DirectionListener {
+    void East();
+    void West();
+    void North();
+    void South();
+}
+
 public class UserController {
     Scanner sc = new Scanner(System.in);
 
     public void Directions() {
 
     }
-    public void Actions() {
-
-    }
-
 
 }
