fe345f879c424f88b30a43717c2e4137dcb8781a
Yazpants <Victoriusify@gmail.com>
2017-03-25 21:19:51 +0100

    Added area description.

--- ProjectBattletrail/src/BattleTrail_2098/FirstTestLevel.java
+++ ProjectBattletrail/src/BattleTrail_2098/FirstTestLevel.java
@@ -28,7 +28,14 @@ public class FirstTestLevel {
      * create all areas and link their exits
      */
     private void createAreas() {
+        GameAreas Bar_69, Slum, Hideout, Hotel, Slum_Street;
 
+        //create the rooms
+        Bar_69 = new GameAreas("in a shady sex dungeon bar called Bar_69");
+        Slum = new GameAreas("in the most vile part of the city");
+        Hideout = new GameAreas("in a gang hideout");
+        Hotel = new GameAreas("in an old empty hotel..seems strange");
+        Slum_Street =  new GameAreas("in the slum area main street");
     }
 
 }
